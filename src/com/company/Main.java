23b9d71bb994e48a01a221588ac01f0afed437c0
mikheevaev1454 <mikheeva@1454.ru>
2020-11-03 15:59:06 +0300

    У меня все получилось

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scr = new Scanner(System.in);
+    System.out.println("Введите два числа и символ выпонения действия над ними");
+    int a, b;
+    String operation;
+    a = scr.nextInt();
+    b = scr.nextInt();
+    operation = scr.next();
+    switch (operation){
+        case "+":
+            System.out.println(a + b);
+            break;
+        case "-":
+            System.out.println(a - b);
+            break;
+        case "*":
+            System.out.println(a * b);
+            break;
+        case "/":
+            if (b == 0) {
+                System.out.println("Данное действие совершить невозможно");
+            }
+            else{
+                System.out.println(a / b);
+            }
+            break;
+        default:
+            System.out.println("Введен неверный знак");
+    }
     }
 }
